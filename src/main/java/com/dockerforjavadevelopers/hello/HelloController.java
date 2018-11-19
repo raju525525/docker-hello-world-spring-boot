42e4d3caa6ecc74faecb066897c1f793b6422aa9
E5000242 <E5000242@IGDCLT0156.internal.innovapptive.com>
2018-11-19 18:53:39 +0530

    heloo

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -8,7 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     
     @RequestMapping("/")
-    public String index() {
+    public String index() throws InterruptedException {
+    	int iteratorValue = 20;
+		System.out.println("\n=================> OOM test started..\n");
+		for (int outerIterator = 1; outerIterator < 20; outerIterator++) {
+			System.out.println("Iteration " + outerIterator + " Free Mem: " + Runtime.getRuntime().freeMemory());
+			int loop1 = 2;
+			int[] memoryFillIntVar = new int[iteratorValue];
+			// feel memoryFillIntVar array in loop..
+			do {
+				memoryFillIntVar[loop1] = 0;
+				loop1--;
+			} while (loop1 > 0);
+			iteratorValue = iteratorValue * 5;
+			System.out.println("\nRequired Memory for next loop: " + iteratorValue);
+			Thread.sleep(1000);
+		}
         return "Hello World\n";
     }
     
