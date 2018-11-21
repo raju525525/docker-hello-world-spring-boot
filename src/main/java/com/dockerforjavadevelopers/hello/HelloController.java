bda2be7c9ecdb6b0ed35419d73c3abe4d5335a8e
raju525525 <raju525525@users.noreply.github.com>
2018-11-21 16:30:52 +0530

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -9,22 +9,8 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() throws InterruptedException {
-    	int iteratorValue = 20;
 		System.out.println("\n=================> OOM test started..\n");
-		for (int outerIterator = 1; outerIterator < 20; outerIterator++) {
-			System.out.println("Iteration " + outerIterator + " Free Mem: " + Runtime.getRuntime().freeMemory());
-			int loop1 = 2;
-			int[] memoryFillIntVar = new int[iteratorValue];
-			// feel memoryFillIntVar array in loop..
-			do {
-				memoryFillIntVar[loop1] = 0;
-				loop1--;
-			} while (loop1 > 0);
-			iteratorValue = iteratorValue * 5;
-			System.out.println("\nRequired Memory for next loop: " + iteratorValue);
-			Thread.sleep(1000);
-		}
-        return "Hello World\n";
+       		 return "Hello World\n";
     }
     
 }
