d65165ff7a3c82a9f295f74bf634ec9802d979a4
E5000242 <E5000242@IGDCLT0156.internal.innovapptive.com>
2018-11-22 10:36:52 +0530

    AutoScaleCode

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -1,16 +1,23 @@
 package com.dockerforjavadevelopers.hello;
 
-
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    
-    @RequestMapping("/")
-    public String index() throws InterruptedException {
+
+	@RequestMapping("/")
+	public String index() throws InterruptedException {
 		System.out.println("\n=================> OOM test started..\n");
-       		 return "Hello World\n";
-    }
-    
+
+		for (int i = 0; i <= 10000000; i++) {
+			int n = 10;
+			String newObject=new String();
+			for (int c = 1; c <= 10; c++) {
+				System.out.println(n + "*" + c + " = " + (n * c));
+			}
+		}
+		return "Hello World\n";
+	}
+
 }
